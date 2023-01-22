5e52c3dd7c4000cac9717c475ec5afcac9c28833
Lela Deslauriers <lela.deslauriers@gmail.com>
2023-01-22 12:06:58 -0800

    filter projects based on menu selector

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
 let aboutDisplayed = false;
+let clickMenu = false;
+const divPortfolio = document.getElementById('project-portfolio');
+
 function filterData(data, key, value) {
     return data.filter(item => item[key] === value);
+}
+
+function displayProjects(data) {
+    data.forEach(project => {
+        //dynamically display projects
+        const divCard = document.createElement('div')
+        divCard.setAttribute('class', 'card')
+        divCard.setAttribute('id', project.name)
+        const h2 = document.createElement('h2')
+        h2.innerText = project.name
+        const img = document.createElement('img')
+        img.src = project.image
+        img.setAttribute('class', 'project-image')
+        const pDescription = document.createElement('p')
+        pDescription.innerText = project.description
+        const pLikes = document.createElement('p')
+        pLikes.innerText = `${project.likes} likes      `
+        const likeButton = document.createElement('button')
+        likeButton.setAttribute('class','like-btn')
+        likeButton.setAttribute('id', project.id)
+        likeButton.innerText = '❤️'
+        divPortfolio.appendChild(divCard);
+        divCard.appendChild(h2);
+        divCard.appendChild(img);
+        divCard.appendChild(pDescription);
+        divCard.appendChild(likeButton);
+        divCard.appendChild(pLikes);
+    });
 }
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -24,7 +55,7 @@ document.addEventListener("DOMContentLoaded", () => {
         return response.json();
     })
     .then(function(data) {
-        const divPortfolio = document.getElementById('project-portfolio');
+  
         const select = document.querySelector('#industry-menu');
         console.log(document.querySelector('#industry-menu'));
         const optionSaaS = document.querySelector('#SaaS');
@@ -34,10 +65,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
         select.addEventListener('click', function(event) {
             console.log(event.target.value)
+
             const filteredProjects = filterData(data, "industry", event.target.value);
+
             console.log(filteredProjects);
 
-            })
+            clickMenu = !clickMenu
+           
+            if (clickMenu) {
+                displayProjects(filteredProjects)
+            } else displayProjects(data)
+        });
 
         //select industry option from menu and filter project data accordingly. can I create a variable and assign the value of the option, and then intropolate the variable to the fetch URL? Then using the selected results, display only those projects. 
 
@@ -49,32 +87,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
         console.log(data);
-        data.forEach(project => {
-            //dynamically display projects
-            const divCard = document.createElement('div')
-            divCard.setAttribute('class', 'card')
-            divCard.setAttribute('id', project.name)
-            const h2 = document.createElement('h2')
-            h2.innerText = project.name
-            const img = document.createElement('img')
-            img.src = project.image
-            img.setAttribute('class', 'project-image')
-            const pDescription = document.createElement('p')
-            pDescription.innerText = project.description
-            const pLikes = document.createElement('p')
-            pLikes.innerText = `${project.likes} likes      `
-            const likeButton = document.createElement('button')
-            likeButton.setAttribute('class','like-btn')
-            likeButton.setAttribute('id', project.id)
-            likeButton.innerText = '❤️'
-            divPortfolio.appendChild(divCard);
-            divCard.appendChild(h2);
-            divCard.appendChild(img);
-            divCard.appendChild(pDescription);
-            divCard.appendChild(likeButton);
-            divCard.appendChild(pLikes);
-        });
-        
     });
 
 
