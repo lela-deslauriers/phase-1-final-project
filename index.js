a00e008a3d3ece8a354b8eeb9b2c5f52a90a8778
Lela Deslauriers <lela.deslauriers@gmail.com>
2023-02-24 14:53:30 -0700

    Created helper function to render cards, chained the appends, refactored select menu, fixed bug in HTML src for js file relative path

--- index.js
+++ index.js
@@ -1,7 +1,55 @@
 let aboutDisplayed = false;
-let clickMenu = false;
+
 const divPortfolioResults = document.getElementById('project-portfolio');
 const divContact = document.getElementById('contact-form');
+
+function updateLikes (pLikes, projectId) {
+
+    let projectLikeCount = parseInt(pLikes.innerText)
+
+    fetch(`http://localhost:3000/projects/${projectId}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+        },
+            body: JSON.stringify(
+                {
+                    "likes": projectLikeCount += 1
+                }
+            )
+        })
+        .then(response => response.json())
+        .then((data) => {
+            pLikes.innerText = `${data.likes} likes`;
+            console.log(projectLikeCount);
+        });
+};
+
+function renderProjectCard(project) {
+    const divCard = document.createElement('div')
+    divCard.setAttribute('class', 'card')
+    divCard.setAttribute('id', project.name)
+    const h2 = document.createElement('h2')
+    h2.innerText = project.name
+    const img = document.createElement('img')
+    img.src = project.image
+    img.setAttribute('class', 'project-image')
+    const pDescription = document.createElement('p')
+    pDescription.innerText = project.description
+    const pIndustry = document.createElement('p')
+    pIndustry.setAttribute('id', project.industry)
+    pIndustry.innerText = `Industry: ${project.industry}`
+    const pLikes = document.createElement('p')
+    pLikes.innerText = `${project.likes} likes      `
+    const likeButton = document.createElement('button')
+    likeButton.setAttribute('class','like-btn')
+    likeButton.setAttribute('id', project.id)
+    likeButton.innerText = '❤️'
+    likeButton.addEventListener('click', ( )=> updateLikes(pLikes, project.id))
+    divPortfolioResults.appendChild(divCard);
+    divCard.append(h2, img, pDescription, pIndustry, likeButton, pLikes);
+}
 
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -27,131 +75,57 @@ document.addEventListener("DOMContentLoaded", () => {
 
         data.forEach(project => {
             //create elements to dynamically display projects
-            const divCard = document.createElement('div')
-            divCard.setAttribute('class', 'card')
-            divCard.setAttribute('id', project.name)
-            const h2 = document.createElement('h2')
-            h2.innerText = project.name
-            const img = document.createElement('img')
-            img.src = project.image
-            img.setAttribute('class', 'project-image')
-            const pDescription = document.createElement('p')
-            pDescription.innerText = project.description
-            const pLikes = document.createElement('p')
-            pLikes.innerText = `${project.likes} likes      `
-            const likeButton = document.createElement('button')
-            likeButton.setAttribute('class','like-btn')
-            likeButton.setAttribute('id', project.id)
-            likeButton.innerText = '❤️'
-            divPortfolioResults.appendChild(divCard);
-            divCard.appendChild(h2);
-            divCard.appendChild(img);
-            divCard.appendChild(pDescription);
-            divCard.appendChild(likeButton);
-            divCard.appendChild(pLikes);
+            renderProjectCard(project)
         })
+    })
+
+})
 
 
 
-        const select = document.querySelector('#industry-menu');
+const select = document.querySelector('#industry-menu');
 
-        select.addEventListener('change', event => {
+select.addEventListener('change', filterProjects);
 
-            const selectedOption = event.target.value;
-            const filteredProjects = filterData(data, "industry", selectedOption);
-            
-            function filterData(data, key, value) {
-                return data.filter(item => item[key] === value);
+function filterProjects () {
+    let filterValue = select.value;
+    const filterdProjects = document.querySelectorAll('.card').forEach(project => {
+        project.innerText.includes(`${filterValue}`)
+        ? project.style.display = ''
+        : project.style.display = 'none';
+    })
+}
+
+divContact.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+
+    fetch ("http://localhost:3000/messages/", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify (
+            {
+                "name": name,
+                "email": email,
+                "message": message
             }
-
-            divPortfolioResults.replaceChildren()
-
-            filteredProjects.forEach(project => {
-                //create elements to dynamically display projects
-                const divCard = document.createElement('div')
-                divCard.setAttribute('class', 'card')
-                divCard.setAttribute('id', project.name)
-                const h2 = document.createElement('h2')
-                h2.innerText = project.name
-                const img = document.createElement('img')
-                img.src = project.image
-                img.setAttribute('class', 'project-image')
-                const pDescription = document.createElement('p')
-                pDescription.innerText = project.description
-                const pLikes = document.createElement('p')
-                pLikes.innerText = `${project.likes} likes      `
-                const likeButton = document.createElement('button')
-                likeButton.setAttribute('class','like-btn')
-                likeButton.setAttribute('id', project.id)
-                likeButton.innerText = '❤️'
-                divPortfolioResults.appendChild(divCard);
-                divCard.appendChild(h2);
-                divCard.appendChild(img);
-                divCard.appendChild(pDescription);
-                divCard.appendChild(likeButton);
-                divCard.appendChild(pLikes);
-
-            //refactor by using displayProjects(filteredProjects);
-            });
-        });
+    )
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        alert("Thanks for submitting your message! I am busy coding so I might not get back to you for a while. Appreciate your patience.");
+    })
+    .catch(error => {
+        // Handle errors
+        console.error("Error:", error);
     });
-
-    divPortfolioResults.addEventListener('click', event => {
-        event.preventDefault()
-
-        let projectId = event.target.id;
-        let projectLikeCount = parseInt(event.target.nextElementSibling.innerText)
-
-        fetch(`http://localhost:3000/projects/${projectId}`, {
-            method: "PATCH",
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json"
-        },
-            body: JSON.stringify(
-                {
-                    "likes": projectLikeCount += 1
-                }
-            )
-        })
-        .then(response => response.json())
-        .then(function(data) {
-            console.log(data);
-        });
-    });
-
-    divContact.addEventListener('submit', (event) => {
-        event.preventDefault()
-    
-        const name = document.getElementById("name").value;
-        const email = document.getElementById("email").value;
-        const message = document.getElementById("message").value;
-    
-        fetch ("http://localhost:3000/messages/", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json"
-        },
-        body: JSON.stringify (
-                {
-                    "name": name,
-                    "email": email,
-                    "message": message
-                }
-        )
-        })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            alert("Thanks for submitting your message! I am busy coding so I might not get back to you for a while. Appreciate your patience.");
-        })
-        .catch(error => {
-            // Handle errors
-            console.error("Error:", error);
-        });
-    });
-
 });
 
 
